3576126a25180f64c94fcf6135d45b8dc52cd1ed
GowthamAbi <gowtham2131ece@gmail.com>
2025-10-10 15:27:43 +0530

    login

--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,11 @@ import express from 'express'
 import router from './routes/authRoutes.js'
 import connectDB from './config/db.js'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 const app=express()
 connectDB()
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
      credentials: true
